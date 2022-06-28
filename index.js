6b5faf9b5b9b6c0a8b0d236f3740309c95299548
wanjirugachunji <wanjirugachunji@gmail.com>
2022-06-28 20:15:55 +0300

    commit tree 82d7725a088cac49e6cea2dd4c9cb1839d8c94ac

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-/* Your Code Here */
+const createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+const createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+
+}
+
+const createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    
+        his.timeInEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+        })
+    
+        return this
+
+    }
+const hoursWorkedOnDate = function(soughtDate){
+    const inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    const outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
@@ -20,4 +70,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
